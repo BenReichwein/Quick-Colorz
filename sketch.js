bdb23379d7d210997a45c33a54ba817894ec1eef
Graham Billington <grahamhbillington@gmail.com>
2020-08-11 12:54:09 -0400

    added message when the timer ends

--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ let timeLeft = 10;
 
 let countdown = setInterval(() => {
     // timer is completed
-    if (timeLeft <= 0) {
+    if (timeLeft < 0) {
         clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = "You are out of time!"
         // this is the end state
     } else {
         // set timer to timeLeft variable
