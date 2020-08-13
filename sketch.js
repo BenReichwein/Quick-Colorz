ce2dd103b6006a897494b06e171f1ce48fab33ea
Graham Billington <grahamhbillington@gmail.com>
2020-08-13 17:15:29 -0400

    started the modes with start button and stuff (unfinished)

--- sketch.js
+++ sketch.js
@@ -3,6 +3,12 @@ const width = 5;
 const height = 5;
 const cells = [];
 let score = -1;
+let timeLeft = 10;
+
+// 0 is idle state
+// 1 is playing state
+// 2 is losing state
+// 3 is winning state
 let mode = 0;
 
 //creates table rows and columns based on width and height
@@ -25,12 +31,37 @@ function generateRandomColor() {
     let randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
     return randomColor;
 }
+
+function startTimer() {
+    // # of seconds
+    timeLeft = 10;
+    
+    // make sure that the user is in the playing state before the countdown starts
+    if (mode === 1) {
+        let countdown = setInterval(() => {
+            // timer is completed
+            if (timeLeft <= 0) {
+                clearInterval(countdown);
+                document.getElementById('countdown').innerHTML = "You are out of time!"
+                reset();
+            } else {
+                // set timer to timeLeft variable
+                document.getElementById('countdown').innerHTML = timeLeft;
+            }
+            timeLeft--;
+        }, 1000)
+    }
+}
+
 function Start() {
+    mode = 1;
     //sets a random color of each table cell
     let colorArr = cells.map(elem => {
         elem.addEventListener('mousedown', () => {
             if(elem.style.backgroundColor === document.getElementById('color').style.backgroundColor) {
-                round()
+                document.getElementById('countdown').innerHTML = "You won! +1 point"
+                Score();
+                return;
             }
         })
         return elem.style.backgroundColor = generateRandomColor()
@@ -40,25 +71,29 @@ function Start() {
     let randomColorLevel = Math.floor(Math.random() * colorArr.length)
 
     document.getElementById('color').style.backgroundColor = colorArr[randomColorLevel]
+    startTimer()
 }
 
-// # of seconds
-let timeLeft = 10;
+function reset() {
+    mode = 0;
 
-let countdown = setInterval(() => {
-    // timer is completed
-    if (timeLeft <= 0) {
-        clearInterval(countdown);
-        document.getElementById('countdown').innerHTML = "You are out of time!"
-        // this is the end state
-        alert('You Lost')
-        mode++;
-    } else {
-        // set timer to timeLeft variable
-        document.getElementById('countdown').innerHTML = `${timeLeft} seconds remaining.`;
-    }
-    timeLeft--;
-}, 1000)
+    // clear colorArr
+    colorArr = [];
+
+    // remove color from colorToFind block
+    document.getElementById('color').style.backgroundColor = "";
+
+    // remove color from cells
+    cells.map(elem => {
+        elem.style.backgroundColor = "";
+    })
+
+    document.getElementById('countdown').innerHTML = "You lost! Press start to play again!";
+
+    document.getElementById('startBtn').addEventListener('click', function() {
+        Start();
+    })
+}
 
 function round() {
     if(timeLeft >= 1) {
@@ -70,13 +105,29 @@ function round() {
 }
 
 function Score() {
+    mode = 0;
+
     score++;
+
     document.getElementById('scorecount').innerHTML = `Score: ${score}`;
+
+    document.getElementById('countdown').innerHTML = "You won! Press start to play again!"
+
+    document.getElementById('startBtn').addEventListener('click', function() {
+        Start();
+    })
 }
 
 if (mode === 0) {
-    Score();
-    Start();
-} else {
-    
+    document.getElementById('startBtn').addEventListener('click', function() {
+        Start();
+    })
+// losing state
+} else if (mode === 2) {
+    reset();
+// winning state
+} else if (mode === 3) {
+    document.getElementById('countdown').innerHTML = "You won! +1 point"
+    mode = 0;
 }
+
