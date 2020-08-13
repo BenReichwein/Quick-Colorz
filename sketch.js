242e7c79cd2e18f29719444399a082d6ff080086
Ben <47262607+BenReichwein@users.noreply.github.com>
2020-08-13 15:36:24 -0400

    Update sketch.js
    Score function

--- sketch.js
+++ sketch.js
@@ -1,20 +1,29 @@
-//sets height and width of table cells
+// Width and Height
 const width = 5;
 const height = 5;
 const cells = [];
+let score = -1;
+let mode = 0;
+
+if (mode === 0) {
+    Score();
+    Start();
+} else {
+
+}
 
 //creates table rows and columns based on width and height
 const table = document.createElement("tbody");
 for (let h = 0; h < height; h++) {
-  const tr = document.createElement("tr");
-  for (let w = 0; w < width; w++) {
-    const td = document.createElement("td");
-    td.dataset.row = h;
-    td.dataset.col = w;
-    cells.push(td);
-    tr.append(td);
-  }
-  table.append(tr);
+    const tr = document.createElement("tr");
+    for (let w = 0; w < width; w++) {
+        const td = document.createElement("td");
+        td.dataset.row = h;
+        td.dataset.col = w;
+        cells.push(td);
+        tr.append(td);
+    }
+    table.append(tr);
 }
 document.getElementById("board").append(table);
 
@@ -23,22 +32,23 @@ function generateRandomColor() {
     let randomColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
 }
-
-//sets a random color of each table cell 
-let colorArr = cells.map(elem => {
-    elem.addEventListener('mousedown', () => {
-        if(elem.style.backgroundColor === document.getElementById('color').style.backgroundColor) {
-            round()
-        }
+function Start() {
+    //sets a random color of each table cell
+    let colorArr = cells.map(elem => {
+        elem.addEventListener('mousedown', () => {
+            if(elem.style.backgroundColor === document.getElementById('color').style.backgroundColor) {
+                round()
+            }
+        })
+        return elem.style.backgroundColor = generateRandomColor()
     })
-    return elem.style.backgroundColor = generateRandomColor()
-})
 
 
-let randomColorLevel = Math.floor(Math.random() * colorArr.length)
+    let randomColorLevel = Math.floor(Math.random() * colorArr.length)
 
-document.getElementById('color').style.backgroundColor = colorArr[randomColorLevel]
-document.getElementById('color').innerHTML = colorArr[randomColorLevel]
+    document.getElementById('color').style.backgroundColor = colorArr[randomColorLevel]
+    document.getElementById('color').innerHTML = colorArr[randomColorLevel]
+}
 
 
 // # of seconds
@@ -51,7 +61,7 @@ let countdown = setInterval(() => {
         document.getElementById('countdown').innerHTML = "You are out of time!"
         // this is the end state
         alert('You Lost')
-        location.reload();
+        mode++;
     } else {
         // set timer to timeLeft variable
         document.getElementById('countdown').innerHTML = `${timeLeft} seconds remaining.`;
@@ -60,8 +70,15 @@ let countdown = setInterval(() => {
 }, 1000)
 
 function round() {
-     if(timeLeft >= 1) {
+    if(timeLeft >= 1) {
         alert('Winner')
-        location.reload();
-     }
+        Score();
+        Start();
+        timeLeft = 10;
+    }
+}
+
+function Score() {
+    score++;
+    document.getElementById('scorecount').innerHTML = `Score: ${score}`;
 }
