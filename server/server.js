6fc439d688009842f3ddb5361656c86501b85aa9
Ian Gelfand <gelfandian@gmail.com>
2020-08-17 12:48:07 -0400

    Heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const socketIO = require('socket.io');
-
+const path = require('path')
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = '/';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use((req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 const io = socketIO(server);
 
