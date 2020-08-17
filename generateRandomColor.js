8b59d18a432d86cd62c6b457de1dd682bd2de80d
Ian Gelfand <gelfandian@gmail.com>
2020-08-17 12:36:36 -0400

    Heroku Deployment

--- generateRandomColor.js
+++
@@ -1,5 +0,0 @@
-//generates random color
-function generateRandomColor() {
-    let randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-    return randomColor;
-}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
 
 const users = {}
 
